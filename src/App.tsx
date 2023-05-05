4fac20ee90c8de28f8c894dc0eb37d5a881528aa
Sergei Krivochenko <admin@krivochenko.ru>
2023-05-05 18:16:34 -0300

    Fix basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { MapPage } from './pages/MapPage';
 
 const router = createHashRouter([
   {
-    path: '',
     element: <Layout className="content">
       <Header/>
       <Outlet/>
@@ -18,7 +17,7 @@ const router = createHashRouter([
       { path: ':address', element: <MapPage /> },
     ],
   },
-], { basename: window.location.href.indexOf('krivochenko.github.io/t11n') !== -1 ? '/t11n' : '/' });
+]);
 
 function App() {
   return <AntdApp>
