838956e0b893c86dc2381555d9d30c788e36b5ac
Sergei Krivochenko <admin@krivochenko.ru>
2023-05-05 16:00:33 -0300

    Fix logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import { TonConnectButton } from '@tonconnect/ui-react';
 import './Header.scss';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 export const Header = () => {
   return <div className="header">
     <Link to={'/'}>
-      <img className="logo" src={'/logo.png'} alt="" />
+      <img className="logo" src={logo} alt="" />
     </Link>
     <TonConnectButton />
   </div>;
